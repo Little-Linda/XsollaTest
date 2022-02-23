ae085319c778fca6ff55f96ac0e3acb1cefd366c
Tengri <E.Tengri@hostco.ru>
2022-02-23 20:29:29 +0500

    style fix

--- src/App.js
+++ src/App.js
@@ -41,19 +41,21 @@ function App() {
     } else {
         return (
             <div className='wrapper'>
-                <header className='block-margin-style'>
-                    <h2 className='cards-title'>Event Listing</h2>
-                </header>
-                <Select items={items} filtered={setFilterItems}/>
+                <div className="test">
+                    <header className='block-margin-style'>
+                        <h2 className='cards-title'>Event Listing</h2>
+                    </header>
+                    <Select items={items} filtered={setFilterItems}/>
 
-                <div className='card-wrapper block-margin-style'>
-                    {filteredItems.map(item => (
-                        <div key={item.id} style={{backgroundImage: `url(${item.image})`}} className='card'>
-                            <span className='date-block'>{item.date.split('.')[0]}</span>
-                            <span className='event-name-block'>{item.name}</span>
-                            <button className={ item.favorite ? 'favorite-btn active' : 'favorite-btn' } onClick={() => addFavorite(item.id)}/>
-                        </div>
-                    ))}
+                    <div className='card-wrapper block-margin-style'>
+                        {filteredItems.map(item => (
+                            <div key={item.id} style={{backgroundImage: `url(${item.image})`}} className='card'>
+                                <span className='date-block'>{item.date.split('.')[0]}</span>
+                                <span className='event-name-block'>{item.name}</span>
+                                <button className={ item.favorite ? 'favorite-btn active' : 'favorite-btn' } onClick={() => addFavorite(item.id)}/>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
         );
